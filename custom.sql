9b3d7a701527f077f1bfdf791378710f0901b4ed
Tobias Silicani <silicani80@gmail.com>
2024-10-12 14:14:01 +0200

    WIP

--- custom.sql
+++ custom.sql
@@ -76,7 +76,7 @@ create or replace function index() returns "text/html" as $$
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>PostgREST + HTMX To-Do List</title>
+      <title>PostgREST + HTMX Pet List</title>
       <!-- Pico CSS for CSS styling -->
       <link href="https://cdn.jsdelivr.net/npm/@picocss/pico@next/css/pico.min.css" rel="stylesheet"/>
       <!-- htmx for AJAX requests -->
@@ -103,9 +103,6 @@ create or replace function index() returns "text/html" as $$
           <div>
         </article>
       </main>
-      <!-- Script for Ionicons icons -->
-      <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-      <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     </body>
     </html>
   $html$;
